d2a73a1f2c2a784331fd05c173662966077b86c7
Nakib00 <2022142@iub.edu.bd>
2023-04-26 21:25:52 +0600

    add convert

--- apps.js
+++ apps.js
@@ -30,16 +30,13 @@ let errorMessageElement = document.getElementById("error");
 //     inoct.value = octal;
 // })
 
-
 // convart decmal to binary
 indec.addEventListener("input", () => {
   let decvalue = parseInt(indec.value);
 
-    if(decvalue){
-        inbin.value = decvalue.toString(2);
-        inhex.value = decimalToHexadecimal(decvalue);
-        inoct.value = decimalToOctal(decvalue);
-    }
+    inbin.value = decvalue.toString(2);
+    inhex.value = decvalue.toString(16);
+    inoct.value = decvalue.toString(8);
 });
 
 // convart binary to decimal, octal, hexadecimal
@@ -48,9 +45,10 @@ inbin.addEventListener("input", () => {
 
   // if binary is valide then convart it to decimal
   if (binvalidator(binvalue)) {
-    indec.value = parseInt(binvalue, 2);
-    inoct.value = binaryToOctal(binvalue);
-    inhex.value = binaryToHexadecimal(binvalue);
+    let dec = parseInt(binvalue, 2);
+    indec.value = dec;
+    inoct.value = dec.toString(8);
+    inhex.value = dec.toString(16);
     errorMessageElement.textContent = "";
   } else {
     errorMessageElement.textContent = "Enter valid binary value";
@@ -68,200 +66,19 @@ function binvalidator(num) {
 }
 
 // Convert octal to decimal, binary, hexadecimal
-inoct.addEventListener("input",() => {
-    let octvalue = parseInt(inoct.value);
+inoct.addEventListener("input", () => {
+  let octvalue = parseInt(inoct.value);
+  let dec = parseInt(octvalue, 8);
 
-    if(octvalue){
-        inbin.value = octalToBinary(octvalue);
-        indec.value = octalToDecimal(octvalue);
-        inhex.value = octalToHexadecimal(octvalue);
-    }
+  indec.value = dec;
+  inbin.value = dec.toString(2);
+  inhex.value = dec.toString(16);
 });
 
 // convart hexadeciamal to decimal, binary, octal
 inhex.addEventListener("input", () => {
-    let hexvalue = inhex;
-
-    if(hexvalue){
-        indec.value = hexadecimalToDecimal(hexvalue);
-        inbin.value = hexadecimalToBinary(hexvalue);
-        inoct.value = hexadecimalToOctal(hexvalue);
-    }
+  let dec = parseInt(inhex.value, 16);
+  indec.value = dec;
+  inoct.value = dec.toString(8);
+  inbin.value = dec.toString(2);
 });
-
-// Decimal to Octal
-function decimalToOctal(decimalNumber) {
-  let octalNumber = 0,
-    i = 1;
-
-  while (decimalNumber !== 0) {
-    octalNumber += (decimalNumber % 8) * i;
-    decimalNumber = Math.floor(decimalNumber / 8);
-    i *= 10;
-  }
-
-  return octalNumber;
-}
-
-// Octal to Decimal function
-function octalToDecimal(octalNumber) {
-    let decimalNumber = 0,
-      i = 0;
-  
-    while (octalNumber !== 0) {
-      decimalNumber += (octalNumber % 10) * Math.pow(8, i);
-      i++;
-      octalNumber = Math.floor(octalNumber / 10);
-    }
-  
-    return decimalNumber;
-}
-
-// Octal to Binary
-function octalToBinary(octalNumber) {
-  let binaryNumber = "",
-    i = 0;
-
-  while (octalNumber !== 0) {
-    let remainder = octalNumber % 10;
-    let binaryDigits = ("000" + remainder.toString(2)).slice(-3);
-    binaryNumber = binaryDigits + binaryNumber;
-    i++;
-    octalNumber = Math.floor(octalNumber / 10);
-  }
-
-  return binaryNumber;
-}
-
-// Binary to octal
-function binaryToOctal(binaryNumber) {
-  let octalNumber = "",
-    i = 0;
-
-  while (binaryNumber.length % 3 !== 0) {
-    binaryNumber = "0" + binaryNumber;
-  }
-
-  while (i < binaryNumber.length) {
-    let binaryDigits = binaryNumber.substr(i, 3);
-    let decimalNumber = parseInt(binaryDigits, 2);
-    octalNumber += decimalNumber.toString(8);
-    i += 3;
-  }
-
-  return octalNumber;
-}
-
-// Decimal to Hexadecimal
-function decimalToHexadecimal(decimalNumber) {
-  let hexadecimalDigits = "0123456789ABCDEF";
-  let hexadecimalNumber = "";
-
-  while (decimalNumber !== 0) {
-    let remainder = decimalNumber % 16;
-    hexadecimalNumber = hexadecimalDigits.charAt(remainder) + hexadecimalNumber;
-    decimalNumber = Math.floor(decimalNumber / 16);
-  }
-
-  return hexadecimalNumber;
-}
-
-// Hexadecimal to Decimal
-function hexadecimalToDecimal(hexadecimalNumber) {
-  let decimalNumber = 0;
-
-  for (let i = 0; i < hexadecimalNumber.length; i++) {
-    let hexadecimalDigit = hexadecimalNumber.charAt(i);
-    let decimalDigit = parseInt(hexadecimalDigit, 16);
-    decimalNumber = decimalNumber * 16 + decimalDigit;
-  }
-
-  return decimalNumber;
-}
-
-//Binary to Hexadecimal
-function binaryToHexadecimal(binaryNumber) {
-  let hexadecimalDigits = "0123456789ABCDEF";
-  let hexadecimalNumber = "";
-
-  while (binaryNumber.length % 4 !== 0) {
-    binaryNumber = "0" + binaryNumber;
-  }
-
-  for (let i = 0; i < binaryNumber.length; i += 4) {
-    let binaryDigits = binaryNumber.substr(i, 4);
-    let decimalNumber = parseInt(binaryDigits, 2);
-    hexadecimalNumber += hexadecimalDigits.charAt(decimalNumber);
-  }
-
-  return hexadecimalNumber;
-}
-
-// Hexadecimal to Binary
-function hexadecimalToBinary(hexadecimalNumber) {
-  let binaryNumber = "";
-
-  for (let i = 0; i < hexadecimalNumber.length; i++) {
-    let hexadecimalDigit = hexadecimalNumber.charAt(i);
-    let decimalNumber = parseInt(hexadecimalDigit, 16);
-    let binaryDigits = decimalNumber.toString(2).padStart(4, "0");
-    binaryNumber += binaryDigits;
-  }
-
-  return binaryNumber;
-}
-
-// Octal to Hexadecimal
-function octalToHexadecimal(octalNumber) {
-  let decimalNumber = 0;
-
-  for (let i = 0; i < octalNumber.length; i++) {
-    let octalDigit = octalNumber.charAt(i);
-    let decimalDigit = parseInt(octalDigit, 8);
-    decimalNumber = decimalNumber * 8 + decimalDigit;
-  }
-
-  let hexadecimalDigits = "0123456789ABCDEF";
-  let hexadecimalNumber = "";
-
-  while (decimalNumber > 0) {
-    let remainder = decimalNumber % 16;
-    hexadecimalNumber = hexadecimalDigits.charAt(remainder) + hexadecimalNumber;
-    decimalNumber = Math.floor(decimalNumber / 16);
-  }
-
-  return hexadecimalNumber;
-}
-
-// Hexadecila to Octal
-function hexadecimalToOctal(hexadecimalNumber) {
-  let decimalNumber = 0;
-
-  for (let i = 0; i < hexadecimalNumber.length; i++) {
-    let hexadecimalDigit = hexadecimalNumber.charAt(i);
-    let decimalDigit = parseInt(hexadecimalDigit, 16);
-    decimalNumber = decimalNumber * 16 + decimalDigit;
-  }
-
-  let octalNumber = "";
-
-  while (decimalNumber > 0) {
-    let remainder = decimalNumber % 8;
-    octalNumber = remainder.toString() + octalNumber;
-    decimalNumber = Math.floor(decimalNumber / 8);
-  }
-
-  return octalNumber;
-}
-
-// Binary to text
-function binaryToText(binary) {
-  let text = "";
-  for (let i = 0; i < binary.length; i += 8) {
-    let byte = binary.substr(i, 8);
-    let charCode = parseInt(byte, 2);
-    let character = String.fromCharCode(charCode);
-    text += character;
-  }
-  return text;
-}
